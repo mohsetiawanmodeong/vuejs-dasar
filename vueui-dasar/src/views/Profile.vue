12ef07a15df69738bd724544888ec9c6d9a41c77
mohsetiawanmodeong <mohsetiawanmodeong@gmail.com>
2022-10-04 16:23:09 +0900

    tambah router vue ui belajar

---
+++ vueui-dasar/src/views/Profile.vue
@@ -0,0 +1,15 @@
+<template>
+   <div class="profile">
+      <h1>This is an profile page</h1>
+   </div>
+</template>
+
+<script>
+
+export default {
+   // eslint-disable-next-line vue/multi-word-component-names
+   name: 'Profile',
+   components: {
+   }
+}
+</script>
