63c77200b0ecb19965593a9b0868be83ddf4b3d2
mohsetiawanmodeong <mohsetiawanmodeong@gmail.com>
2022-10-05 11:58:00 +0900

    add profile vue ui

--- vueui-dasar/src/views/Profile.vue
+++ vueui-dasar/src/views/Profile.vue
@@ -1,10 +1,30 @@
 <template>
    <div class="profile">
-      <h1>This is an profile page</h1>
+      <div class="container">
+         <div class="row justify-content-center">
+            <div class="col-lg-6">
+               <div class="d-flex text-start">
+                  <img src="../assets/minestar.png" class="user-pic me-3" />
+                  <div>
+                     <h5 class="mt-0">
+                        CAT&#174; MINESTAR&#8482; SYSTEM
+                     </h5>
+                     <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                        blanditiis soluta, praesentium, facilis repellendus!
+                     </p>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+
    </div>
 </template>
 
 <script>
+
+import 'bootstrap/dist/css/bootstrap.css'
 
 export default {
    // eslint-disable-next-line vue/multi-word-component-names
@@ -13,3 +33,11 @@ export default {
    }
 }
 </script>
+
+<style scoped>
+.user-pic {
+   border-radius: 200%;
+   height: 90px;
+   width: 90px;
+}
+</style>
